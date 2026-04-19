e5b9da0a2c0d6bcba967806d0a2fbd41b301f4ae
Medeiros281213 <gustavosfmf28@gmail.com>
2026-04-19 15:42:03 -0300

    Adiciona estrutura inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+function adicionar() {
+  const input = document.getElementById("tarefa");
+  const lista = document.getElementById("lista");
+
+  if (input.value === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  lista.appendChild(li);
+  input.value = "";
+}
